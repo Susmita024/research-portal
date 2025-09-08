1b2b22e2167e45c86eed7f753cbf00dad240b324
tawhidmonowar <susmitapurkayastha89@gmail.com>
2025-09-08 22:57:11 +0600

    Create footer.blade.php

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="bg-gray-800 text-white p-4 mt-8 shadow-inner">
+    <div class="container mx-auto text-center">
+        <p>&copy; {{ date('Y') }} Research Portal | Some rights reserved.</p>
+        <p>Developed by <a href="https://github.com/Susmita024">Susmita Purkayastha</a>.</p>
+    </div>
+</footer>
